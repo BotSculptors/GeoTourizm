3860f2f62b3e8c51fa7355a035a8c81a905a8130
Surik266 <surik.pozoyan@gmail.com>
2018-08-09 11:20:53 +0400

    add main tour page V_1.0.0

--- app/components/Trips/Trips.js
+++ app/components/Trips/Trips.js
@@ -1,69 +1,125 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import {fetchAllTrips, fetchTrip} from '../../actions/actions'
-import { Link } from 'react-router-dom'
-
+import {Link} from 'react-router-dom'
 
 export class Trips extends React.Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            trips: [],
-            baseURL: props.match.path + '/'
-        };
+    this.state = {
+      trips: [],
+      baseURL: props.match.path + '/'
+    };
 
-    }
+  }
 
+  // Fetch trips once component did mount
+  componentDidMount() {
+    this.props.dispatch(fetchAllTrips());
+  }
 
-    // Fetch trips once component did mount
-    componentDidMount(){
-        this.props.dispatch( fetchAllTrips() );
-    }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      trips: nextProps.trips.length
+        ? nextProps.trips
+        : this.state.trips
+    });
+  }
 
-    componentWillReceiveProps(nextProps){
-        this.setState({
-            trips: nextProps.trips.length ? nextProps.trips : this.state.trips
-        });
-    }
+  render() {
 
+    const {trips, baseURL} = this.state;
 
+    return (<div>
+      <div className='titleSection'>
+        <h1>Туры в Грузию :
+        </h1>
+      </div>
+      <div className='Trips'>
+        {/*<div className='gridBlockOfTrip'>
 
-    render() {
+    <Link to={"/trips"} className='title'>
+      Тур 2
+    </Link>
 
-        const {trips, baseURL} = this.state;
+    <div className='overview'></div>
 
+  </div>
+  <div className='gridBlockOfTrip'>
 
-        return (
+    <Link to={"/trips"} className='title'>
+      Тур 3
+    </Link>
 
-            <div>
+    <div className='overview'></div>
 
-                <div className='titleSection'><h1>Trips</h1></div>
+  </div> */
+        }
 
+        {
+          trips.map((trip, t) => (<div key={t} className='gridBlockOfTrip'>
 
-                {trips.map((trip, t) => (
-                    <div key={t} className='gridBlockOfTrip'>
+            <Link to={baseURL + trip.page_url} className='title'>
+              <div className="img_trip">
+                <img src="https://image.ibb.co/kO7TWd/sataflia_13_1_g_MEp_L.jpg"></img>
+              </div>
+              <div className="trips_info">
+                <div className="location_trip">
+                  <i class="material-icons">
+                    location_on
+                  </i>
+                  <span>Кахетия.Лагодехи</span>
+                </div>
 
+                <div className="title_trip">
+                  <span>{trip.title}</span>
+                </div>
+              </div>
 
-                        <Link to={baseURL + trip.page_url} className='title'>{trip.title}</Link>
-                        <div className='overview'></div>
-                    </div>
-                ))}
+            </Link>
+            <div className='overview'>
+              <div className="derections_trip">
+                <span>{trip.directions}</span>
+              </div>
+              <div className="data_trip">
+                <div className="price_trip">
+                  <i class="material-icons">
+                    attach_money
+                  </i>
+                  <span>Price</span>
+                </div>
 
+                <div className="time_trip">
+
+                  <i class="material-icons">
+                    access_time
+                  </i>
+                  <span>Time
+                  </span>
+                </div>
+
+                <div className="type_trip">
+                  <i class="material-icons">
+                    public
+                  </i>
+                  <span>Type</span>
+                </div>
+              </div>
 
             </div>
-        );
-    }
 
-
+          </div>))
+        }
+      </div>
+    </div>);
+  }
 
 }
 
-
 const mapStateToProps = (state, ownProps) => {
-    return Object.assign({}, ownProps, {trips: state.tripsReducer});
+  return Object.assign({}, ownProps, {trips: state.tripsReducer});
 };
-
 
 export default connect(mapStateToProps)(Trips);
